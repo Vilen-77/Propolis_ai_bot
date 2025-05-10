693db60687134a3fd4d8a9dea528f829961bb255
Vilen-77 <5741400@gmail.com>
2025-05-10 21:32:34 +0300

    Update commands.py

--- modules/commands.py
+++ modules/commands.py
@@ -1,17 +1,10 @@
-# Импортируем необходимые классы
-from telegram import Update  # Объект Update содержит сообщение от пользователя
-from telegram.ext import ContextTypes, CommandHandler, Application  # Обработчик и тип контекста
+from telegram import Update
+from telegram.ext import ContextTypes, CommandHandler, Application
 
-# Функция, которая будет вызвана, когда пользователь отправит команду /start
+# Обработчик команды /start
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # Ответим пользователю простым сообщением
     await update.message.reply_text("Привет! Я Telegram-бот, подключённый через вебхук 🚀")
 
-
-# Функция для добавления обработчиков в приложение бота
+# Регистрируем все команды
 def add_handlers(application: Application):
-    # Создаём обработчик команды /start и связываем его с функцией start_command
-    start_handler = CommandHandler("start", start_command)
-
-    # Добавляем обработчик в приложение
-    application.add_handler(start_handler)
+    application.add_handler(CommandHandler("start", start_command))
